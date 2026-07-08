17a9873b7d0eb88c064e80864167dfaa7ae72c9a
Debfront <deborah_monteiro12@hotmail.com>
2026-07-08 09:14:43 -0300

    feat: desenvolvimento dos componentes da task board

---
+++ src/app/features/category/view/category/category.component.ts
@@ -0,0 +1,28 @@
+import { CategoryService } from './../../services/category.service';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MainListComponent } from '../../components/main-list/main-list.component';
+import { ColorsListComponent } from '../../components/colors-list/colors-list.component';
+import { AsyncPipe } from '@angular/common';
+
+const COMPONENTS = [MainListComponent, ColorsListComponent];
+
+const PIPES = [AsyncPipe];
+
+@Component({
+  selector: 'app-category',
+  standalone: true,
+  imports: [...COMPONENTS, ...PIPES],
+  template: `
+    <div class="flex flex-col justify-between itens-center h-full w-full">
+      <!-- main-list -->
+      <app-main-list />
+      <!-- colors-list -->
+      <app-colors-list />
+    </div>
+  `,
+  styles: '',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CategoryComponent {
+  private readonly categoryService = inject(CategoryService);
+}
---
+++ src/app/layout/main/main.component.ts
@@ -0,0 +1,22 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { CategoryComponent } from '../../features/category/view/category/category.component';
+import { TaskComponent } from '../../features/task/task/task.component';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [CategoryComponent, TaskComponent, MatDividerModule],
+  template: `
+    <div class="h-screen flex w-full">
+      <app-category class="w-1/4" />
+
+      <mat-divider class="h-full opacity-50" vertical />
+
+      <app-task class="w-3/4" />
+    </div>
+  `,
+  styles: '',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MainComponent {}
